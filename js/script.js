bc9aff1d14934fe1cf4228214ed19f90d6bce824
François Revol <revol@free.fr>
2016-02-25 17:12:12 +0100

    Add console redirection to a div for Android
    Since Android's browser doesn't have a console, this helps a bit.
    
    Commented out.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 /* RadioTimer, Copyright 2016 François Revol */
 
+/* Android doesn't really have a console...
+cf. http://stackoverflow.com/questions/6604192/showing-console-errors-and-alerts-in-a-div-inside-the-page
+*/
+/*
+if (/Android/.test(navigator.userAgent)) {
+	if (typeof console  != "undefined")
+		if (typeof console.log != 'undefined')
+			console.olog = console.log;
+		else
+			console.olog = function() {};
+
+	console.log = function(message) {
+		console.olog(message);
+		$('#debugDiv').append('<p>' + message + '</p>');
+	};
+	console.error = console.debug = console.info =  console.log
+}
+*/
+
 var buttonChars = {
 	"eject":"⏏",
 	"session_prev":"⏮",
