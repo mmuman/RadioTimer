534d8fedccd2bf51ae55ec7077684ba3b8bf7ffe
François Revol <revol@free.fr>
2021-04-03 23:48:57 +0200

    Add a crude script to sync to OBS to start/stop recording

---
+++ obs-sync.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+port="8010"
+
+if [ "$#" -lt 1 ]; then
+	echo "Synchronize OBS recording on 127.0.0.1:$port on GNU/Linux (X11 only)"
+	echo "Usage: $0 shortcut"
+	echo "shortcut is whatever key you set in OBS, like R"
+	exit 1
+fi
+
+key="$1"
+
+
+while true; do printf 'HTTP/1.1 100 OK\n\n' | nc -q 0 -l "$port"; xdotool search --name "OBS 26" windowactivate --sync %1 key "$key" windowactivate $(xdotool getactivewindow); done
