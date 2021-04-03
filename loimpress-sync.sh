3d15131d582b35b12f5dfdc005036cd800512c78
François Revol <revol@free.fr>
2021-04-03 23:49:26 +0200

    Simple script to trigger next slide in LibreOffice Impress
    We fake Linux-Show-Player

---
+++ loimpress-sync.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# cf. https://wiki.documentfoundation.org/Development/Impress_Remote_Protocol
+
+if [ "$#" -lt 1 ]; then
+	echo "Synchronize with LibreOffice Impress in lieu of Linux-Show-Timer (next slide)"
+	echo "Usage: $0 pin"
+	echo "Use [LSP:0] to get the next slide"
+	exit 1
+fi
+
+lspport=8070
+name=RadioTimer
+port=1599
+pin="$1"
+
+(
+	#echo "GET / \r\n\r\n";
+	sleep 1
+	echo "LO_SERVER_CLIENT_PAIR\n$name\n$pin\n\n";
+	while true; do printf 'HTTP/1.1 100 OK\n\n' | nc -q 0 -l "$lspport"; echo "transition_next\n\n"; done
+) | nc 127.0.0.1 "$port"
